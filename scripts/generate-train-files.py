9a9817273feb6aa1e1a3fd83f4ede3bcb78f5f31
Joseph Turian <turian@gmail.com>
2010-02-11 17:06:10 -0500

    Can now read in examples files.

--- scripts/generate-train-files.py
+++ scripts/generate-train-files.py
@@ -6,8 +6,17 @@ TRAIN_FILENAME = "../data/train/CategorizedQuerySample.txt"
 # Read labels
 import string
 labels = []
+origlabel_to_newlabel = {}
 for l in open(CATEGORIES_FILENAME):
     l = string.strip(l)
+    origl = l
     l = string.replace(l, "\\", ":")
     l = string.replace(l, " ", "_")
     labels.append(l)
+    origlabel_to_newlabel[origl] = l
+
+for e in open(TRAIN_FILENAME):
+    v = string.split(string.strip(e), "\t")
+    query = v[0]
+    labels = [origlabel_to_newlabel[string.strip(l)] for l in v[1:]]
+    print labels, query
