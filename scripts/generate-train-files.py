22824ce6d21f6b18e5d0b1bc05bf327433cc45b7
Joseph Turian <turian@gmail.com>
2010-02-11 17:21:22 -0500

    generate-train-files.py now generates basic training features

--- scripts/generate-train-files.py
+++ scripts/generate-train-files.py
@@ -1,22 +1,60 @@
 #!/usr/bin/env python
+"""
+TODO: Add different example weighting.
+"""
 
-CATEGORIES_FILENAME = "../data/train/Categories.txt"
-TRAIN_FILENAME = "../data/train/CategorizedQuerySample.txt"
+import sys, string
+from os.path import join
+import os.path, os
+from os.path import join
+BASEDIR = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 
-# Read labels
-import string
-labels = []
-origlabel_to_newlabel = {}
-for l in open(CATEGORIES_FILENAME):
-    l = string.strip(l)
-    origl = l
-    l = string.replace(l, "\\", ":")
-    l = string.replace(l, " ", "_")
-    labels.append(l)
-    origlabel_to_newlabel[origl] = l
+CATEGORIES_FILENAME = join(BASEDIR, "data/train/Categories.txt")
+TRAIN_FILENAME = join(BASEDIR, "data/train/CategorizedQuerySample.txt")
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-n", "--name", dest="name", help="Name of this run")
+(options, args) = parser.parse_args()
+assert len(args) == 0
+assert options.name is not None
+
+all_labels, origlabel_to_newlabel = None, None
+def read_labels():
+    global all_labels, origlabel_to_newlabel
+
+    # Read labels
+    all_labels = []
+    origlabel_to_newlabel = {}
+    for l in open(CATEGORIES_FILENAME):
+        l = string.strip(l)
+        origl = l
+        l = string.replace(l, "\\", ":")
+        l = string.replace(l, " ", "_")
+        all_labels.append(l)
+        origlabel_to_newlabel[origl] = l
+
+
+
+workdir = join(BASEDIR, "work/%s" % options.name)
+print >> sys.stderr, "Working in directory: %s" % workdir
+if not os.path.exists(workdir): os.mkdir(workdir)
+assert os.path.isdir(workdir)
+
+read_labels()
+
+label_to_trainfile = {}
+for l in all_labels:
+    trainfile = open(join(workdir, "trainfeature.%s.txt" % l), "wt")
+    label_to_trainfile[l] = trainfile
 
 for e in open(TRAIN_FILENAME):
     v = string.split(string.strip(e), "\t")
     query = v[0]
     labels = [origlabel_to_newlabel[string.strip(l)] for l in v[1:]]
-    print labels, query
+
+    for l in all_labels:
+        if l in labels: outl = l
+        else: outl = "NOT-%s" % l
+        label_to_trainfile[l].write("%s %s\n" % (outl, query))
+#    print labels, query
